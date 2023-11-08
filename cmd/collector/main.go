b724447b67de10c5b4b5d041da850246d821b230
Kamil Wcisło <mek.xgt@gmail.com>
2023-11-08 22:25:47 +0100

    main: add initial hardcoded logic for app

--- cmd/collector/main.go
+++ cmd/collector/main.go
@@ -1,5 +1,75 @@
 package main
 
+import (
+	"bytes"
+	"log"
+	"os"
+	"time"
+
+	yaml "github.com/goccy/go-yaml"
+	"gitlab.com/mek_x/DataCollector/pkg/datastore"
+	"gitlab.com/mek_x/DataCollector/pkg/dispatcher/cron"
+	"gitlab.com/mek_x/DataCollector/pkg/parser/jsonpath"
+	"gitlab.com/mek_x/DataCollector/pkg/sink/stdout"
+	"gitlab.com/mek_x/DataCollector/pkg/source/mqtt"
+)
+
 func main() {
-	return
+	if len(os.Args) < 2 {
+		log.Fatal("need config file!")
+	}
+
+	y, err := os.ReadFile(os.Args[1])
+	if err != nil {
+		log.Fatal("can't read config file: ", err)
+	}
+
+	ds := datastore.New()
+
+	params := struct {
+		Url  string `yaml:"url"`
+		User string `yaml:"user"`
+		Pass string `yaml:"pass"`
+	}{}
+
+	path, err := yaml.PathString("$.sources.mqtt.params")
+	if err != nil {
+		log.Fatal("path string is bad: ", err)
+	}
+
+	err = path.Read(bytes.NewReader(y), &params)
+	if err != nil {
+		log.Fatal("can't parse source params")
+	}
+
+	path, err = yaml.PathString("$.data.outside.path")
+	if err != nil {
+		log.Fatal("path string is bad: ", err)
+	}
+
+	var topic string
+	err = path.Read(bytes.NewReader(y), &topic)
+	if err != nil {
+		log.Fatal("can't parse source params")
+	}
+
+	mqtt := mqtt.NewClient(params.Url, params.User, params.Pass)
+	mqtt.Start()
+
+	p := jsonpath.New("out", ds)
+	p.AddVar("temp", "$.T")
+	p.AddVar("humi", "$.H")
+	p.AddVar("addr", "$.address")
+
+	mqtt.AddDataSource(topic, p)
+
+	d := cron.New("*/1 * * * * *", ds)
+	d.AddSink("out", stdout.Stdout{})
+	d.Start()
+
+	for i := 0; i < 10; i++ {
+		time.Sleep(1 * time.Second)
+	}
+
+	mqtt.End()
 }
--- go.mod
+++ go.mod
@@ -6,14 +6,20 @@ require (
 	github.com/PaesslerAG/jsonpath v0.1.1
 	github.com/eclipse/paho.mqtt.golang v1.4.3
 	github.com/go-co-op/gocron v1.35.3
+	github.com/goccy/go-yaml v1.11.2
 )
 
 require (
 	github.com/PaesslerAG/gval v1.0.0 // indirect
+	github.com/fatih/color v1.10.0 // indirect
 	github.com/google/uuid v1.4.0 // indirect
 	github.com/gorilla/websocket v1.5.0 // indirect
+	github.com/mattn/go-colorable v0.1.8 // indirect
+	github.com/mattn/go-isatty v0.0.12 // indirect
 	github.com/robfig/cron/v3 v3.0.1 // indirect
 	go.uber.org/atomic v1.9.0 // indirect
 	golang.org/x/net v0.8.0 // indirect
 	golang.org/x/sync v0.1.0 // indirect
+	golang.org/x/sys v0.6.0 // indirect
+	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
 )
